1997800d0adaa76d5bf138c6b6a0ae79979d635a
Isabell Kiral-Kornek <isa.kiral@gmx.de>
2018-02-07 17:49:01 +1100

    bar chart

--- app.py
+++ app.py
@@ -6,37 +6,78 @@ import dash_core_components as dcc
 import dash_html_components as html
 import plotly.graph_objs as go
 
-df = pandas.read_csv("connection_strength_norm.csv")
+df_original = pandas.read_csv("connection_strength_norm.csv")
+df_original = df_original[df_original.value >= 0.2]
 
-G = nx.convert_matrix.from_pandas_edgelist(df, 'ingredients', 'variable', edge_attr='value')
-pos = nx.spring_layout(G)
+df_regiondata = pandas.read_csv("occurrences_per_cuisine_remove_unused_nodes_normalised.csv")
+
+ingredients = df_original.ingredients
+
+dd_options = [] 
+for i in ingredients:
+    dd_options.append({'label': i, 'value': i})
+
 app = dash.Dash()
-
 app.layout = html.Div([
+    dcc.Dropdown(
+        id='ing_dd',
+        options=dd_options,
+        value=dd_options[0]['label']),
     dcc.Graph(id='network-graph'),
     dcc.Slider(
         id='strength-slider',
-        max=df['value'].max(),
-        value=df['value'].min(),
+        max=df_original['value'].max(),
+        value=df_original['value'].min(),
         step=None,
-        marks={str(value): str(value) for value in df['value'].unique()}
-    )
+        marks={str(value): str(value) for value in df_original['value'].unique()}
+    ),
+    dcc.Graph(id='bar-graph') 
+
 ])
 
+@app.callback(
+    dash.dependencies.Output('bar-graph', 'figure'), 
+    [dash.dependencies.Input('ing_dd', 'value')
+    ])
+def update_bargraph(selected_ingredient):
+    bar_traces = []
+    bar_traces.append(go.Bar(
+        x = list(df_regiondata['Cuisine']),
+        y = df_regiondata[selected_ingredient]))
+    return {
+        'data': bar_traces,
+        'layout': go.Layout(
+            autosize=True,
+            titlefont=dict(size=16),
+            showlegend=False,
+            height=1000,
+            hovermode='closest'
+        )
+    }
 
 @app.callback(
     dash.dependencies.Output('network-graph', 'figure'),
-    [dash.dependencies.Input('strength-slider', 'value')])
-def update_figure(selected_strength):
+    [
+    dash.dependencies.Input('strength-slider', 'value'),
+    dash.dependencies.Input('ing_dd','value')
+    ])
+def update_figure(selected_strength, selected_ingredient):
+
+    df = df_original[df_original.value >= selected_strength]
+
+    G = nx.convert_matrix.from_pandas_edgelist(df, 'ingredients', 'variable', edge_attr='value')
+
+    pos = nx.spring_layout(G)
+
     traces = []
     # Draw edges
     edge_trace = go.Scattergl(
         x=[],
         y=[],
         text=[],
-        line=go.Line(width=0.5, color='rgba(0, 0, 0, 0.8'),
+        line=go.Line(width=2, color='rgba(0, 0, 0, 0.4'),
         hoverinfo='text',
-        mode='lines'
+        mode='lines',
     )
     for edge in G.edges(data=True):
         if edge[2]['value'] > selected_strength:
@@ -47,6 +88,7 @@ def update_figure(selected_strength):
             edge_trace['text'].append(edge[2]['value'])
 
     # Draw nodes
+    colorarray = []
     node_trace = go.Scattergl(
         x=[],
         y=[],
@@ -55,9 +97,10 @@ def update_figure(selected_strength):
         hoverinfo='text',
         marker=go.Marker(
             size=20,
+            color=colorarray,
             line=dict(
                 width=2,
-                color='rgba(0, 0, 0, .8)'
+                color='rgba(0, 0, 0, .2)'
             )
         )
     )
@@ -66,6 +109,10 @@ def update_figure(selected_strength):
         node_trace['x'].append(x)
         node_trace['y'].append(y)
         node_trace['text'].append(node)
+        if node == selected_ingredient:
+            colorarray.append('red')
+        else:
+            colorarray.append('orange')
 
     traces.append(edge_trace)
     traces.append(node_trace)
